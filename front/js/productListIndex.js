cdd44a6023ba50345650b0d3bfe94cba3b0e637e
Wa-ran <mgs.gilles@gmail.com>
2020-10-19 11:34:38 +0200

    constructeur pour type de produits > waran.js

--- front/js/productListIndex.js
+++ front/js/productListIndex.js
@@ -1,34 +1,34 @@
-// Affichage des cartes produits
+// Affichage des cartes produits de l'Index -----------------------------------------------------
 let main = document.getElementsByTagName('main')[0];
 let menuProduct = document.getElementById('menuProduct');
 
 const productList = (title , url) => {
-
+// Container
   let container = document.createElement('div');
   container.className = "container border border-primary rounded my-5";
   main.appendChild(container);
-
+// Titre du groupe de produits
   let titleGroup = document.createElement('div');
   titleGroup.className = "border border-primary rounded bg-white p-1 titleGroup";
   titleGroup.innerHTML = '<div><h2 class="h3 text-center m-2" id="' + title + '">' + title + '</h2></div>';
   container.appendChild(titleGroup);
-
+// Row
   let row = document.createElement('div');
   row.className = "row pb-1";
   container.appendChild(row);
-
+// Carte de chaque produits, voir Ajax.js
   ajaxGet(url).then(JSON.parse).then(function(resolve) {
     for (object of resolve) {
       let name = object.name;
       let imageUrl = object.imageUrl;
       let col = document.createElement('div');
+      col.id = title;
       col.className = "col-12 col-sm-6 col-md-4 col-lg-3";
-      col.innerHTML = '<div class="card my-2"><a class="card-body btn btn-outline-primary stretched-link p-0" href="./' + name + '.html"><div style="background-image: url(' + imageUrl + ');" class="card-img-top mb-2" alt=""></div><h5 class="card-title">' + name + '</h5></a></div>';
+      col.innerHTML = '<div class="card my-2 border-white"><a class="card-body btn btn-outline-primary stretched-link p-0" href="./' + name + '.html"><div style="background-image: url(' + imageUrl + ');" class="card-img-top mb-2" alt=""></div><h5 class="card-title">' + name + '</h5></a></div>';
       row.appendChild(col); 
     };
   });
 };
-
-productList('vcam', 'http://localhost:3000/api/cameras');
-productList('oak','http://localhost:3000/api/furniture');
-productList('teddy', 'http://localhost:3000/api/teddies');
+for (type of typeList) {
+  productList(type.title, type.url)
+};
--- front/js/waran.js
+++ front/js/waran.js
@@ -1,4 +1,42 @@
-// Bouton haut de page
+// Constructeur pour faciliter la création et l'utilisation de nouveaux types de produits -----------------------------------------------------------
+let typeList = [];
+class Type {
+  constructor(title, url) {
+    this.title = title;
+    this.url = url;
+  }
+};
+const newType = (title, url) => {
+  let thisNewType = new Type (title, url);
+  typeList.push(thisNewType);
+};
+let vcam = newType('Caméras vintage', 'http://localhost:3000/api/cameras');
+let oak = newType('Meubles en chêne', 'http://localhost:3000/api/furniture');
+let teddy = newType('Ours en peluche fait mains', 'http://localhost:3000/api/teddies');
+// Un array avec les 3 types de produits et les chemins d'accès vers les API est créé
+
+
+// Ancres pour la liste produits du header ------------------------------------------------------------
+const ancresList = (title) => {
+  let ancre = document.createElement('a');
+  ancre.className = "dropdown-item rounded px-1";
+  ancre.href = './index.html#' + title;
+  ancre.innerHTML = title;
+  menuProduct.appendChild(ancre);
+}
+for (type of typeList) {
+  ancresList(type.title);
+}
+
+
+
+ajaxGet('http://localhost:3000/api/cameras').then(JSON.parse).then(function(response) {
+  console.log(response)
+})
+
+
+
+// Bouton haut de page ------------------------------------------------------
 let pageUp = document.getElementById('pageUp');
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -16,18 +54,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-
-
-// Ancres produits du header
-const ancresList = (title) => {
-
-  let ancre = document.createElement('a');
-  ancre.className = "dropdown-item rounded px-1";
-  ancre.href = './index.html#' + title;
-  ancre.innerHTML = title;
-  menuProduct.appendChild(ancre);
-}
-ancresList('vcam');
-ancresList('oak');
-ancresList('teddy');
